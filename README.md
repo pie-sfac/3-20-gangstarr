078077a0aef4180e181dca49d953881527f60bda
0SCAR0421 <oscar0421@kakao.com>
2023-07-17 17:32:56 +0900

    initalize project

---
+++ README.md
@@ -0,0 +1,12 @@
+# Team Gangstarr
+
+** 반갑습니다 여러분 Team Gangstarr 입니다. **
+
+# 포인티 디자인 시스템 라이브러리
+
+## 실행방법
+
+```bash
+npm install
+npm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Reset } from "styled-reset";
+
+function App() {
+  return (
+    <>
+      <Reset />
+    </>
+  );
+}
+
+export default App;
