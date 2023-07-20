10452143e67dfc8a8c625932caeaf27435cc17d0
0SCAR0421 <oscar0421@kakao.com>
2023-07-20 12:59:50 +0900

    Feat #2 컬러 정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Reset } from 'styled-reset';
-import Headline from './Test/Headline';
 
 function App() {
   return (
     <>
       <Reset />
-      <Headline>asd</Headline>
     </>
   );
 }
