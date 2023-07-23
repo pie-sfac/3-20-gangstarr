da059067157e17be50cddc8a2c07cb413a81a8df
0SCAR0421 <oscar0421@kakao.com>
2023-07-23 21:29:28 +0900

    Feat #18 버튼 컴포넌트 스타일링 및 기능구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import { Reset } from 'styled-reset';
-import { Icon } from './components/icon';
+import { styled } from 'styled-components';
+import Button from './components/Button';
+
+const ButtonBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  padding: 50px;
+`;
 
 function App() {
+  const handleClick = () => {
+    console.log('hello');
+  };
+
   return (
     <>
       <Reset />
-      <Icon name='add'></Icon>
+      <ButtonBox>
+        <Button size='full' onClick={handleClick}>
+          텍스트
+        </Button>
+      </ButtonBox>
     </>
   );
 }
