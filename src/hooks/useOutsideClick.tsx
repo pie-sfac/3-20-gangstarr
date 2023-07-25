a6374fe9e9a3e9913ba40eba0c6b0c01483fb74a
0SCAR0421 <oscar0421@kakao.com>
2023-07-25 14:12:25 +0900

    Fix #20 event type 수정

--- src/hooks/useOutsideClick.tsx
+++ src/hooks/useOutsideClick.tsx
@@ -1,30 +1,25 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 const useOutsideClick = (callback: () => void) => {
   const ref = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const handleClick = (event: React.MouseEvent<HTMLDivElement>): void => {
-      if (
-        ref.current &&
-        !ref.current.contains(event.target as HTMLDivElement)
-      ) {
+    const handleClick = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as HTMLElement)) {
         callback();
       }
     };
 
-    const handleInputEsc = (
-      event: React.KeyboardEvent<HTMLDivElement>,
-    ): void => {
+    const handleInputEsc = (event: KeyboardEvent) => {
       if (event.code === 'Escape') callback();
     };
 
-    document.addEventListener('keydown', () => handleInputEsc);
-    document.addEventListener('mousedown', () => handleClick);
+    document.addEventListener('keydown', handleInputEsc);
+    document.addEventListener('mousedown', handleClick);
 
     return () => {
-      document.removeEventListener('keydown', () => handleInputEsc);
-      document.removeEventListener('mousedown', () => handleClick);
+      document.removeEventListener('keydown', handleInputEsc);
+      document.removeEventListener('mousedown', handleClick);
     };
   }, [callback]);
 
