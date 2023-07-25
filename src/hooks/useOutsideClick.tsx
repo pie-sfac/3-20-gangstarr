38d42baf2da7d149d4afc5b284fd3ad15bae882f
0SCAR0421 <oscar0421@kakao.com>
2023-07-25 13:47:58 +0900

    Fix: click, keyboard event 타입 지정

--- src/hooks/useOutsideClick.tsx
+++ src/hooks/useOutsideClick.tsx
@@ -1,25 +1,30 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const useOutsideClick = (callback: () => void) => {
   const ref = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const handleClick = (event: any) => {
-      if (ref.current && !ref.current.contains(event.target)) {
+    const handleClick = (event: React.MouseEvent<HTMLDivElement>): void => {
+      if (
+        ref.current &&
+        !ref.current.contains(event.target as HTMLDivElement)
+      ) {
         callback();
       }
     };
 
-    const handleInputEsc = (event: any) => {
+    const handleInputEsc = (
+      event: React.KeyboardEvent<HTMLDivElement>,
+    ): void => {
       if (event.code === 'Escape') callback();
     };
 
-    document.addEventListener('keydown', handleInputEsc);
-    document.addEventListener('mousedown', handleClick);
+    document.addEventListener('keydown', () => handleInputEsc);
+    document.addEventListener('mousedown', () => handleClick);
 
     return () => {
-      document.removeEventListener('keydown', handleInputEsc);
-      document.removeEventListener('mousedown', handleClick);
+      document.removeEventListener('keydown', () => handleInputEsc);
+      document.removeEventListener('mousedown', () => handleClick);
     };
   }, [callback]);
 
