f121a23fd554bc49c04ba1f88691f30e9da8037b
jennie-0530 <95169983+jennie-0530@users.noreply.github.com>
2023-07-26 13:52:27 +0900

    Update label.ts

--- src/styles/label.ts
+++ src/styles/label.ts
@@ -3,37 +3,19 @@ import { font } from '.';
 
 export const labelStyles = {
   small: {
-    boxSizing: 'border-box',
     width: '54px',
     height: '21px',
     borderRadius: '4px',
-    padding: '4px 8px',
-    display: 'inline-flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: '10px',
   },
   medium: {
-    boxSizing: 'border-box',
     width: '62px',
     height: '21px',
     borderRadius: '4px',
-    padding: '4px 8px',
-    display: 'inline-flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: '10px',
   },
   large: {
-    boxSizing: 'border-box',
     width: '64px',
     height: '23px',
     borderRadius: '100px',
-    padding: '4px 8px',
-    display: 'inline-flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: '10px',
   },
   background: {
     blue: {
@@ -64,13 +46,11 @@ export const labelStyles = {
     },
   },
   caption1: {
-    fontFamily: font.fontFamily,
     fontSize: font.fontSize.fontSize12,
     fontWeight: font.fontWeight.fontWeightRegular,
     lineHeight: font.lineHeight.lineHeight128,
   },
   caption2: {
-    fontFamily: font.fontFamily,
     fontSize: font.fontSize.fontSize10,
     fontWeight: font.fontWeight.fontWeightRegular,
     lineHeight: font.lineHeight.lineHeight128,
