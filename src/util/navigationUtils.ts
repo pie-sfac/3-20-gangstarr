8f1b185f8891de158fbb3cf2e10c949e610fd80b
dong-fa <120082854+dong-fa@users.noreply.github.com>
2023-07-31 16:16:42 +0900

    fix #16 console.log 삭제

--- src/util/navigationUtils.ts
+++ src/util/navigationUtils.ts
@@ -1,7 +1,5 @@
 export const getPath = (target: string): string | undefined => {
   const { pathname } = new URL(window.location.href);
-  console.log(pathname);
   const currentpath = pathname.split('/');
-  console.log(currentpath);
   return currentpath[1] ? currentpath[1] : 'home';
 };
