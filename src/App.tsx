b90029123251002b5e4033a65cbeac760d4db580
0SCAR0421 <oscar0421@kakao.com>
2023-07-31 14:49:21 +0900

    Fix #34 App.tsx 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { Item } from './components/Item';
 import PoinTStyleProvider from './styles/StyleProvider';
 
 function App() {
-  return (
-    <>
-      <PoinTStyleProvider>
-        <Item></Item>
-        <Item fill='outline'></Item>
-      </PoinTStyleProvider>
-    </>
-  );
+  return <PoinTStyleProvider></PoinTStyleProvider>;
 }
 
 export default App;
