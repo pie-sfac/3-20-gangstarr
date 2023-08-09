0d500795423cd3f872e73e2033d8794d36113720
jennie <mikjk0530@gmail.com>
2023-08-09 18:04:42 +0900

    Feat #73 radio 완성

--- src/lib/styles/GlobalStyle.tsx
+++ src/lib/styles/GlobalStyle.tsx
@@ -1,11 +1,13 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, styled } from 'styled-components';
 import { color } from './color';
 import check from '../assets/check.svg';
 
 const GlobalStyle = createGlobalStyle`
-  *, *::before, *::after{
+  *,
+  *::before,
+  *::after {
     margin: 0;
-    padding:0;
+    padding: 0;
     font-family: 'Roboto', 'Pretendard';
     box-sizing: border-box;
   }
@@ -40,11 +42,60 @@ const GlobalStyle = createGlobalStyle`
 
     &:disabled {
       background: ${color.gray200};
-      border: 1px solid ${color.gray300}; 
+      border: 1px solid ${color.gray300};
     }
 
     &:disabled:checked {
       background: ${color.gray300};
+    }
+  }
+
+  input[type='radio'] {
+    appearance: none;
+    -webkit-appearance: none;
+    width: 20px;
+    height: 20px;
+    border-radius: 100%;
+    position: relative;
+    border: 1px solid ${color.borderLine300};
+    background: ${color.basicWhite};
+
+    &:checked {
+      border: 1px solid ${color.primary300};
+
+      &::after {
+        content: '';
+        display: inline-block;
+        position: absolute;
+        width: 12px;
+        height: 12px;
+        background: ${color.primary300};
+        border-radius: 100%;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
+    }
+    &:disabled {
+      background: ${color.gray200};
+      border: 1px solid ${color.gray300};
+    }
+
+    &:disabled:checked {
+      background: ${color.basicWhite};
+
+      &::after {
+        content: '';
+        display: inline-block;
+        width: 12px;
+        height: 12px;
+        background: ${color.gray300};
+        border-radius: 100%;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
     }
   }
 `;
